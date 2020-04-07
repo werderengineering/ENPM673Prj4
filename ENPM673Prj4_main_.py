24376a92a0610c1d187352f285346ff34fa18782
Werder <werdertimothy@gmail.com>
2020-04-07 15:08:29 -0600

    Video files imported

--- ENPM673Prj4_main_.py
+++ ENPM673Prj4_main_.py
@@ -6,6 +6,8 @@ import imutils
 import os
 import random
 
+from imageFileNames import *
+
 print('Imports Complete')
 
 print('CV2 version')
@@ -17,7 +19,54 @@ prgRun = True
 
 
 def main(prgRun):
-    print("Hello!")
+    problem =3
+
+    if problem==1:
+        directory = './Bolt2/img'
+        # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
+
+        print("Getting images from " + str(directory))
+        imageList = imagefiles(directory)  # get a stack of images
+
+        """process each image individually"""
+        for i in range(len(imageList)):
+            frameDir = directory + '/' + imageList[i]
+            frame = cv2.imread(frameDir)
+
+            cv2.imshow('Working Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+    if problem==2:
+        directory = './Car4/img'
+        # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
+
+        print("Getting images from " + str(directory))
+        imageList = imagefiles(directory)  # get a stack of images
+
+        """process each image individually"""
+        for i in range(len(imageList)):
+            frameDir = directory + '/' + imageList[i]
+            frame = cv2.imread(frameDir)
+
+            cv2.imshow('Working Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+    if problem==3:
+        directory = './DragonBaby/img'
+        # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
+
+        print("Getting images from " + str(directory))
+        imageList = imagefiles(directory)  # get a stack of images
+
+        """process each image individually"""
+        for i in range(len(imageList)):
+            frameDir = directory + '/' + imageList[i]
+            frame = cv2.imread(frameDir)
+
+            cv2.imshow('Working Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
 
 
     prgRun = False
