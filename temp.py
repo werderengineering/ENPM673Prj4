275a3869535d89439de534b5e45a82efa4002e7b
Cheng <chengc0611@gmail.com>
2020-04-13 20:58:08 -0400

    Inverse compositional algorithm 3.0 algorithm setup and need more testing Cheng

--- temp.py
+++ temp.py
@@ -1,20 +1,27 @@
 import cv2
 import numpy as np
+import imagesProcessing as ip
+
 
 def rectAndTemp_problem1(flag=False):
+
+
+def rectAndTemp_problem2(flag=False):
     img1 = cv2.imread('./Car4/img/0001.jpg')
     xl = 65
     xu = 180
     yl = 45
     yu = 135
     rect = np.array([[xl, xu], [yl, yu], [1, 1]])
-    temp = img1[yl:yu,xl:xu]
+    temp = ip.subImageByBoundingBox(img1, rect)
     temp = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
     if flag:
         cv2.imwrite('./Car4/template.jpg', temp)
         if cv2.waitKey(0):
             cv2.destroyAllWindows()
     return rect, temp
+
+
 
 
 def rectAndTemp_problem2(flag=False):
