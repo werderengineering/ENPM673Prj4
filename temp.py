007e7c3ed8ab68404d2065086eff981e2c8f9ec2
hfang743 <58123475+hfang743@users.noreply.github.com>
2020-04-13 21:06:32 -0400

    temp.py update

--- temp.py
+++ temp.py
@@ -2,19 +2,33 @@ import cv2
 import numpy as np
 import imagesProcessing as ip
 
-
 def rectAndTemp_problem1(flag=False):
-
+    frame = cv2.imread('./Bolt2/img/0001.jpg')
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    xl = 250
+    xu = 320
+    yl = 75
+    yu = 150
+    rect = np.array([[xl, xu], [yl, yu], [1, 1]])
+    # temp = img[yl:yu, xl:xu]
+    temp = ip.subImageByBoundingBox(img, rect)
+    if flag:
+        cv2.imwrite('./Bolt2/template.jpg', temp)
+        if cv2.waitKey(0):
+            cv2.destroyAllWindows()
+    return rect, temp
 
 def rectAndTemp_problem2(flag=False):
-    img1 = cv2.imread('./Car4/img/0001.jpg')
+    frame = cv2.imread('./Car4/img/0001.jpg')
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
     xl = 65
     xu = 180
     yl = 45
     yu = 135
     rect = np.array([[xl, xu], [yl, yu], [1, 1]])
-    temp = ip.subImageByBoundingBox(img1, rect)
-    temp = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
+    temp = ip.subImageByBoundingBox(img, rect)
     if flag:
         cv2.imwrite('./Car4/template.jpg', temp)
         if cv2.waitKey(0):
@@ -22,19 +36,18 @@ def rectAndTemp_problem2(flag=False):
     return rect, temp
 
 
-
-
-def rectAndTemp_problem2(flag=False):
-    img1 = cv2.imread('./Bolt2/img/0001.jpg')
-    xl = 250
-    xu = 320
+def rectAndTemp_problem3(flag=False):
+    frame = cv2.imread('./DragonBaby/img/0001.jpg')
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    xl = 130
+    xu = 220
     yl = 75
-    yu = 150
+    yu = 300
     rect = np.array([[xl, xu], [yl, yu], [1, 1]])
-    temp = img1[yl:yu, xl:xu]
-    temp = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
+    temp = ip.subImageByBoundingBox(img, rect)
     if flag:
-        cv2.imwrite('./Bolt2/template.jpg', temp)
+        cv2.imwrite('./DragonBaby/template.jpg', temp)
         if cv2.waitKey(0):
             cv2.destroyAllWindows()
     return rect, temp
