4e2bd547cb6d452c9e18dfc8c79e08ad542e4c2f
Cheng <chengc0611@gmail.com>
2020-04-16 21:05:52 -0400

    Inverse compositional algorithm 4.0 algorithm setup and need more testing Cheng

--- ENPM673Prj4_main_.py
+++ ENPM673Prj4_main_.py
@@ -7,10 +7,12 @@ import os
 import random
 from scipy import signal
 
-import featureObjectTracking as ft
+import featureRegistration as fr
 import imagesProcessing as ip
+import temp
 
 from imageFileNames import *
+# from OpticalFlow import *
 
 print('Imports Complete')
 
@@ -20,28 +22,6 @@ print(cv2.__version__)
 flag = False
 prgRun = True
 flag_trackingFeature = True
-
-
-def createtemplate(directory, rect):
-    frame = cv2.imread(directory+'/0001.jpg')
-    left = rect[0]
-    right = rect[1]
-    top = rect[2]
-    bottom = rect[3]
-    template = frame[top:bottom, left: right]
-    # template = frame[left: right,top:bottom]
-
-    rect= np.array([[left, right], [bottom, top], [1, 1]])
-
-
-    cv2.imshow('template Frame', template)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        None
-
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    # template = cv2.bitwise_and(template,225,255)
-
-    return template,rect
 
 def devPar(Par, rect):
     xD = rect[1] - rect[0]
@@ -61,92 +41,89 @@ def devPar(Par, rect):
 
     return newPar
 
-def runVidOp(directory, rect):
 
-    tempt,rect=createtemplate(directory, rect)
-    frames = ip.firstTemplateAndSecondFrameAndImageList(
-        directory)  # get first bounding box around template, template, frames
+def runVid(directory, rect):
+    Pprev = np.zeros([6, 1])
+    Pprev[4] = rect[0]
+    Pprev[5] = rect[2]
 
-    p = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).reshape(6, 1)
-    print("Import images done")
-    if flag:
-        ip.drawRect(frames[0], rect, True)
-    for frame in frames:
-        # try:
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.equalizeHist(frame_gray)
-        rect,p = ft.affineLKtracker(frame_gray, tempt, rect,p)  # rect update
-        tempt = ip.subImageByBoundingBox(frame_gray, rect)  # template update
-        """show results"""
-        frame_featureMarked = ip.drawRect(frame, rect, False)
-        if flag_trackingFeature:
-            cv2.imshow("Tracking feature", frame_featureMarked)
+    print("Getting images from " + str(directory))
+    imageList = imagefiles(directory)  # get a stack of images
+
+    """process each image individually"""
+    for i in range(len(imageList)):
+        frameDir = directory + '/' + imageList[i]
+        frame = cv2.imread(frameDir)
+
+        if i == 0:
+            template = createtemplate(frame, rect)
+
+        if i > 1:
+            PframeDir = directory + '/' + imageList[i - 1]
+            Pframe = cv2.imread(PframeDir)
+
+            framG = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            templateG = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+            # Grad = cv2.Laplacian(framG, cv2.CV_64F)
+
+            GradX = cv2.Sobel(framG, cv2.CV_64F, 1, 0, ksize=5)
+            GradY = cv2.Sobel(framG, cv2.CV_64F, 0, 1, ksize=5)
+
+            Tcx, Tcy, Pprev = affineLKtracker(framG, templateG, rect, Pprev, GradX, GradY)
+
+            cv2.circle(frame, (Tcx, Tcy), 10, (0, 0, 255), 2)
+
+            # Pprev = devPar(Pprev, rect)
+
+            cv2.imshow('Original Frame', frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
-        # except:
-        #     print(rect)
-        #     pass
-
-
-# #Deprecated
-# def runVid(directory, rect):
-#     Pprev = np.zeros([6, 1])
-#     Pprev[4] = rect[0]
-#     Pprev[5] = rect[2]
-#
-#     print("Getting images from " + str(directory))
-#     imageList = imagefiles(directory)  # get a stack of images
-#
-#     """process each image individually"""
-#     for i in range(len(imageList)):
-#         frameDir = directory + '/' + imageList[i]
-#         frame = cv2.imread(frameDir)
-#
-#         if i == 0:
-#             template = createtemplate(frame, rect)
-#
-#         if i > 1:
-#             PframeDir = directory + '/' + imageList[i - 1]
-#             Pframe = cv2.imread(PframeDir)
-#
-#             framG = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#             templateG = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-#
-#             # Grad = cv2.Laplacian(framG, cv2.CV_64F)
-#
-#             GradX = cv2.Sobel(framG, cv2.CV_64F, 1, 0, ksize=5)
-#             GradY = cv2.Sobel(framG, cv2.CV_64F, 0, 1, ksize=5)
-#
-#             Tcx, Tcy, Pprev = affineLKtracker(framG, templateG, rect, Pprev, GradX, GradY)
-#
-#             # Pprev = devPar(Pprev, rect)
-#
-#             cv2.imshow('Original Frame', frame)
-#             if cv2.waitKey(25) & 0xFF == ord('q'):
-#                 break
-
 
 
 def main(prgRun):
     # start file
-    problem = 3
+    problem = 2
 
     if problem == 1:
         directory = './Bolt2/img'
         # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
         # left = 250, right = 320, top = 75, bottom = 150
-
-        rect=[250,320,75,150]
-        runVidOp(directory,rect)
+        # rect = [250, 320, 75, 150]
+        # runVid(directory, rect)
+        rect_template, tempt, frames = temp.rectAndTemp_problem1(directory)  # get first bounding box around template, template, frames
+        print("Import images done")
+        rect = rect_template.copy()
+        transformation_affine = np.eye(3,
+                                       3)  # since it's the warp transformation of first image itself, it should change the coordinates
+        cache = []
+        if flag:
+            ip.drawRect(frames[0], rect, True)
+        for frame in frames:
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame_gray = cv2.equalizeHist(frame_gray)
+            rect, transformation_affine, cache = ft.affineLKtracker(frame_gray, rect, tempt, rect_template,
+                                                                    transformation_affine, cache,
+                                                                    algorithm="inverse Compositional",
+                                                                    flag_itera=True)  # rect update
+            ip.drawRect(frame, rect, flag_showImgFeatureWithMarked=True)
+        cv2.destroyAllWindows()
         print("Problem 1 finished")
         # runVid(directory, rect)
+
 
     if problem == 2:
         directory = './Car4/img'
         # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
         # left = 65, right = 180, top = 45, bottom = 135
-        rect = [67, 165, 50, 130]
-        runVidOp(directory, rect)
+        # rect = [65, 180, 45, 135]
+        # runVid(directory, rect)
+        rect_template, tempt, frames = temp.rectAndTemp_problem2(directory, flag=True)  # get first bounding box around template, template, frames
+        print("Import images done")
+        ip.drawRect(frames[0], rect_template, True)
+        if cv2.waitKey(0):
+            cv2.destroyAllWindows()
+        fr.LKRegisteration(frames, tempt, rect_template, flag_showFeatureRegisteration=True)
         print("Problem 2 finished")
         # runVid(directory, rect)
 
@@ -154,10 +131,10 @@ def main(prgRun):
         directory = './DragonBaby/img'
         # directory = str(input('What is the name and directory of the folder with the images? Note, this should be entered as"./folder_name if on Windows": \n'))
         # left = 130, right = 220, top = 75, bottom = 300
-        rect = [130,220, 75, 300]
-        runVidOp(directory, rect)
-        print("Problem 3 finished")
+        # rect = [130, 220, 75, 300]
+
         # runVid(directory, rect)
+        rect_template, tempt, frames = temp.rectAndTemp_problem3(directory)  # get first bounding box around template, template, frames
 
     prgRun = False
     return prgRun
